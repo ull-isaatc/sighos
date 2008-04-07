831e20e6ec7c5e1f5d577be084f54892a1440878
Ivan Castilla <ivan@isaatc.ull.es>
2008-04-07 17:37:05 +0000

    Separado proyecto para las utilidades: - simkit.random (Clases aleatorias añadidas) - es.ull.isaatc.function - es.ull.isaatc.random - es.ull.isaatc.util

--- simulator/src/es/ull/isaatc/util/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides miscellaneous utility classes, such as simulation cycle management 
- * ({@link es.ull.isaatc.util.Cycle}, {@link es.ull.isaatc.util.CycleIterator}...), 
- * complex data structures ({@link es.ull.isaatc.util.PrioritizedMap}, 
- * {@link es.ull.isaatc.util.OrderedList}), and other utilities 
- * ({@link es.ull.isaatc.util.ClassPathHacker}, {@link es.ull.isaatc.util.RandomPermutation}).
- */
-package es.ull.isaatc.util;
