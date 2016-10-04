c9a01d1e1a5567189f749294b0864b4a3a1928d2
icasrod <icasrod@ull.edu.es>
2016-10-04 12:09:46 +0100

    Adding first classes to the Port project

---
+++ PSIGHOS-Port/src/es/ull/iis/simulation/port/Container.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package es.ull.iis.simulation.port;
+
+/**
+ * @author Iván Castilla
+ *
+ */
+public class Container extends PortObject {
+
+}
---
+++ PSIGHOS-Port/src/es/ull/iis/simulation/port/PortObject.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package es.ull.iis.simulation.port;
+
+/**
+ * A generic class to encapsulate any important actor or passive component of the port. 
+ * @author Iván Castilla
+ *
+ */
+public class PortObject {
+
+}
