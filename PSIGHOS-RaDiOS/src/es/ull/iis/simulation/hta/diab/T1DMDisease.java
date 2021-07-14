2a46af91c613083baebffb0b14d85a50702c175d
icasrod <icasrod@ull.edu.es>
2021-07-14 22:39:46 +0100

    Updating

--- PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/diab/T1DMDisease.java
+++ PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/diab/T1DMDisease.java
@@ -8,11 +8,10 @@ import es.ull.iis.simulation.hta.diab.manifestations.EndStageRenalDisease;
 import es.ull.iis.simulation.hta.diab.manifestations.Macroalbuminuria;
 import es.ull.iis.simulation.hta.diab.manifestations.Microalbuminuria;
 import es.ull.iis.simulation.hta.diab.manifestations.SevereHypoglycemiaEvent;
-import es.ull.iis.simulation.hta.outcomes.UtilityCalculator.DisutilityCombinationMethod;
+import es.ull.iis.simulation.hta.params.SecondOrderCostParam;
 import es.ull.iis.simulation.hta.params.SecondOrderParamsRepository;
-import es.ull.iis.simulation.hta.progression.Disease;
-import es.ull.iis.simulation.hta.progression.DiseaseProgression;
 import es.ull.iis.simulation.hta.progression.Manifestation;
+import es.ull.iis.simulation.hta.progression.StagedDisease;
 import es.ull.iis.simulation.hta.progression.Transition;
 import es.ull.iis.util.Statistics;
 import simkit.random.RandomVariateFactory;
@@ -21,11 +20,28 @@ import simkit.random.RandomVariateFactory;
  * @author Iván Castilla
  *
  */
-public class T1DMDisease extends Disease {
+public class T1DMDisease extends StagedDisease {
+	public static class DEF_C_DNC {
+		/** Value computed by substracting the burden of complications from the global burden of DM1 in Spain; 
+		 * finally divided by the prevalent DM1 population */
+		public final static double VALUE = (5809000000d - 2143000000d) / 3282790d;
+		/** The year of the default cost for diabetes with no complications: for updating with IPC */
+		public final static int YEAR = 2012; 
+		/** Description of the source */
+		public final static String SOURCE = "Crespo et al. 2012: http://dx.doi.org/10.1016/j.avdiab.2013.07.007";
+	}
 	private static final String STR_HBA1C = "HbA1c";
 	private static final double P_DNC_ALB1 = 0.0436;
 	private static final double[] CI_DNC_ALB1 = {0.0136, 0.0736}; // Assumption
 	private static final double BETA_ALB1 = 3.25;
+	private static final double P_DNC_ALB2 = 0.0037;
+	private static final double BETA_ALB2 = 7.95;
+	private static final double P_ALB1_ESRD = 0.0133;
+	private static final double[] CI_ALB1_ESRD = {0.01064, 0.01596};
+	private static final double P_ALB2_ESRD = 0.1579;
+	private static final double P_ALB1_ALB2 = 0.1565;
+	private static final double P_DNC_ESRD = 0.0002;
+
 	final private Manifestation she;
 	final private Manifestation alb1;
 	final private Manifestation alb2;
@@ -49,42 +65,28 @@ public class T1DMDisease extends Disease {
 
 	@Override
 	public void registerSecondOrderParameters() {
+		secParams.addCostParam(new SecondOrderCostParam(secParams, SecondOrderParamsRepository.STR_COST_PREFIX + "DNC", "Cost of Diabetes with no complications", 
+				DEF_C_DNC.SOURCE, DEF_C_DNC.YEAR, 
+				DEF_C_DNC.VALUE, SecondOrderParamsRepository.getRandomVariateForCost(DEF_C_DNC.VALUE)));
+		
 		final double[] paramsDNC_ALB1 = Statistics.betaParametersFromNormal(P_DNC_ALB1, Statistics.sdFrom95CI(CI_DNC_ALB1));
 		secParams.addProbParam(getNullManifestation(), alb1, 
 				"https://www.sheffield.ac.uk/polopoly_fs/1.258754!/file/13.05.pdf", 
 				P_DNC_ALB1, RandomVariateFactory.getInstance("BetaVariate", paramsDNC_ALB1[0], paramsDNC_ALB1[1]));
 		secParams.addRRParam(getNullManifestation(), alb1, 
 				"DCCT 1996 https://doi.org/10.2337/diab.45.10.1289", BETA_ALB1); 
-	}
 
-	@Override
-	public DiseaseProgression getProgression(Patient pat) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double getAnnualCostWithinPeriod(Patient pat, double initAge, double endAge) {
-		// TODO Auto-generated method stub
-		return 0;
+		final double[] paramsALB1_ESRD = Statistics.betaParametersFromNormal(P_ALB1_ESRD, Statistics.sdFrom95CI(CI_ALB1_ESRD));
 	}
 
 	@Override
 	public double getDiagnosisCost(Patient pat) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getAnnualTreatmentAndFollowUpCosts(Patient pat, double initAge, double endAge) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getDisutility(Patient pat, DisutilityCombinationMethod method) {
-		// TODO Auto-generated method stub
-		return 0;
+		return secParams.getCostParam(SecondOrderParamsRepository.STR_COST_PREFIX + "DNC", pat.getSimulation());
 	}
 
 	/**
