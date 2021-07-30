82c31b0941e4398e1414a550ca7ec002c710b3db
Iván Castilla Rodríguez <icasrod@ull.edu.es>
2021-07-30 12:52:01 +0100

    Simply put a comment and a new FIXME to StagedDisease

--- PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/progression/TimeToEventCalculator.java
+++ PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/progression/TimeToEventCalculator.java
@@ -6,5 +6,10 @@ import es.ull.iis.simulation.hta.Patient;
  * Computes the time to event for each patient
  */
 public interface TimeToEventCalculator {
+	/**
+	 * Returns the simulation timestamp when an event is intended to happen
+	 * @param pat A patient
+	 * @return the simulation timestamp when an event is intended to happen
+	 */
 	public long getTimeToEvent(Patient pat);
 }
