21e36bea1db7f5e8659c656618eb08ee21d1a39a
masbe <masbe@192.168.1.51>
2022-05-01 21:44:55 +0100

    Updated set of constants to deal with the ontology

--- PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/osdi/DiseaseBuilder.java
+++ PSIGHOS-RaDiOS/src/es/ull/iis/simulation/hta/osdi/DiseaseBuilder.java
@@ -4,14 +4,32 @@ import org.w3c.xsd.owl2.Ontology;
 
 import es.ull.iis.ontology.radios.Constants;
 import es.ull.iis.ontology.radios.json.schema4simulation.Disease;
+import es.ull.iis.simulation.hta.Patient;
 import es.ull.iis.simulation.hta.params.SecondOrderParamsRepository;
 import es.ull.iis.simulation.hta.progression.StandardDisease;
 
 public class DiseaseBuilder {
 	public static StandardDisease getDiseaseInstance(Ontology ontology, SecondOrderParamsRepository secParams, String diseaseName) {
 		
-		StandardDisease disease = new StandardDisease(secParams, diseaseName);
-		disease.setName(diseaseName);
+		StandardDisease disease = new StandardDisease(secParams, diseaseName,OwlHelper.getDataPropertyValue(diseaseName, OSDiNames.DataProperty.HAS_DESCRIPTION.getName())) {
+
+			@Override
+			public void registerSecondOrderParameters() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public double getDiagnosisCost(Patient pat) {
+				return 0;
+			}
+
+			@Override
+			public double getAnnualTreatmentAndFollowUpCosts(Patient pat, double initAge, double endAge) {
+				return 0;
+			}
+			
+		};
 		disease.setBirthPrevalence(OwlHelper.getDataPropertyValue(diseaseName, Constants.DATAPROPERTY_BIRTH_PREVALENCE));
 
 		disease.setScreeningStrategies(null);
